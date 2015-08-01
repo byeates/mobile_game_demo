84011b09268a27e4692aa6c7680caab631d1a641
Bennett Yeates <bennett.yeates@gmail.com>
2015-08-01 12:12:06 -0500

    Updated to new unity 5.1 networking

---
+++ Assets/Scripts/PlayerNetwork.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerNetwork : NetworkBehaviour 
+{
+	[SerializeField] Camera FPSCharacterCamera;
+	[SerializeField] AudioListener audioListener;
+
+	// Use this for initialization
+	void Start () 
+	{
+		if ( isLocalPlayer )
+		{
+			GameObject.Find("SpectatorCamera").SetActive(false);
+			GetComponent<CharacterController>().enabled = true;
+			GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = true;
+			FPSCharacterCamera.enabled = true;
+			audioListener.enabled = true;
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerSend.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerSend : NetworkBehaviour 
+{
+	[SyncVar]
+	private Vector3 position;
+
+	[SerializeField] Transform playerTransform;
+	[SerializeField] float lerpBy = 15.0f;
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		SendPosition();
+		LerpPosition();
+	}
+
+	/** Interpolate users positions to resolve stutter effect */
+	void LerpPosition()
+	{
+		if ( !isLocalPlayer )
+		{
+			playerTransform.position = Vector3.Lerp( playerTransform.position, position, Time.deltaTime * lerpBy );
+		}
+	}
+
+	/** Server command to send position updates */
+	[Command]
+	void CmdUpdatePosition( Vector3 pos )
+	{
+		if ( isLocalPlayer )
+		{
+			position = pos;
+		}
+	}
+
+	[ClientCallback]
+	void SendPosition()
+	{
+		if ( isLocalPlayer )
+		{
+			CmdUpdatePosition( playerTransform.position );
+		}
+	}
+}
