5c21a3d2b025e5ffbac1de02c3031dd296904177
Drew <drew@heidgerken.org>
2015-08-25 21:50:47 -0500

    Touch Testing

---
+++ Assets/Scripts/TouchMover.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchMover : MonoBehaviour {
+
+	private float 	turnSpeed;
+	private float 	goSpeed;
+	private float 	thrust;
+	private float	maxSpeed = 15f;
+	private Vector2 touchStart;
+	
+	private bool 	isTap;
+	private bool 	isMoving;
+	
+	public Rigidbody body;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.touchCount > 0) {
+			
+			TouchPhase phase = Input.GetTouch (0).phase;
+			
+			switch(phase)
+			{
+			case TouchPhase.Began:
+				isTap 			= true;
+				isMoving 		= true;
+				touchStart.y 	= Input.GetTouch (0).position.y;
+				touchStart.x 	= Input.GetTouch (0).position.x;
+				turnSpeed 		= 0;
+				break;
+			case TouchPhase.Moved:
+				isTap	= false;
+				goSpeed = Input.GetTouch (0).position.y - touchStart.y;
+				if (goSpeed < 0){
+					goSpeed*=.5f;
+				}
+				turnSpeed = .005f * (Input.GetTouch (0).position.x - touchStart.x);
+				break;
+			case TouchPhase.Stationary:
+				break;
+			case TouchPhase.Ended:
+				if (isTap)
+				{
+					jump ();
+				}
+				isMoving = false;
+				Invoke("noGo",.05f);
+				break;
+			case TouchPhase.Canceled:
+				break;
+			}
+			go();
+		}
+
+	}
+
+	//movement functions
+	void go (){
+		transform.Rotate (0f, turnSpeed, 0f);
+		if (body.velocity.y < .1 && body.velocity.y > -.1) {
+			body.velocity = new Vector3 (.7f * body.velocity.x, body.velocity.y, .7f * body.velocity.z);
+			body.AddForce (transform.forward * goSpeed);
+			
+		} else {
+			body.AddForce (transform.forward * goSpeed*.01f);
+		}
+		capSpeed ();
+	}
+	
+	void noGo(){
+		if (!isMoving && body.velocity != new Vector3(0f,0f,0f)){
+			body.angularVelocity 		= new Vector3(.9f*body.angularVelocity.x,.9f*body.angularVelocity.y,.9f*body.angularVelocity.z);
+			body.velocity 				= new Vector3(.6f*body.velocity.x,body.velocity.y,.6f*body.velocity.z);
+		}
+	}
+	
+	void capSpeed(){
+		if (body.velocity.x > maxSpeed)
+			body.velocity = new Vector3 (maxSpeed, body.velocity.y, body.velocity.z);
+		if (body.velocity.z > maxSpeed)
+			body.velocity = new Vector3 (body.velocity.x, body.velocity.y, maxSpeed);
+		if (body.velocity.x < -1*maxSpeed)
+			body.velocity = new Vector3 (-1*maxSpeed, body.velocity.y, body.velocity.z);
+		if (body.velocity.z < -1*maxSpeed)
+			body.velocity = new Vector3 (body.velocity.x, body.velocity.y, -1*maxSpeed);
+	}
+	
+	void jump(){
+		if (body.velocity.y < .1 && body.velocity.y > -.1) {
+			body.AddForce(new Vector3(0f,700f,0f));
+			body.AddForce (transform.forward * 500f);
+		}
+	}
+}
---
+++ Assets/WalkerBrain.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkerBrain : MonoBehaviour {
+
+	public float turnSpeed = 	2.0f;
+	public float speed = 		10.0f;
+	// delta time
+	private float _dt;
+	private float oldHorz;
+	private float oldVert;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		_dt = Time.deltaTime;
+
+
+		if (Input.GetAxis ( "Horizontal" ) >= .9 || Input.GetAxis ( "Horizontal" ) <= -.9 || Input.GetAxis ( "Horizontal" ) != oldHorz)
+		{
+			Turn ();
+		}
+		if (Input.GetAxis ( "Vertical" ) >= .9 || Input.GetAxis ( "Vertical" ) != oldVert)
+		{
+			Forward ();
+		}
+		if (Input.GetAxis ( "Vertical" ) <= -.9 || Input.GetAxis ( "Vertical" ) != oldVert)
+		{
+			Back ();
+		}
+		if (Input.GetButtonDown ("Jump")) {
+			Jump();
+		}
+
+
+		Debug.Log ("turn: " + Input.GetAxis ( "Vertical" ));
+		Debug.Log ("move: " + Input.GetAxis ( "Horizontal" ));
+
+
+
+
+
+	}
+
+	public void Forward()
+	{
+		transform.Translate( 0, 0, speed * Time.deltaTime );
+		oldVert = Input.GetAxis ( "Vertical" );
+	}
+	
+	public void Back()
+	{
+		transform.Translate( 0, 0, -speed * Time.deltaTime );
+		oldVert = Input.GetAxis ( "Vertical" );
+	}
+	
+	public void Turn()
+	{
+		transform.Rotate (0f, Input.GetAxis ( "Horizontal" ), 0f);
+		oldHorz = Input.GetAxis ( "Horizontal" );
+	}
+
+	public void Jump()
+	{
+			Debug.Log ("JUMP");
+		GetComponent<Rigidbody>().AddForce(0,50000f,0);
+
+	}
+}
